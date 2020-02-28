ac07e10a30396a4bcac800f163d3226012c28153
KyleCole01 <kyle@misfitselectronics.com>
2020-02-28 12:14:37 -0600

    login finished

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,6 +8,15 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(res => {
+        console.log("i am get color", res);
+        setColorList(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }, []);
 
   return (
     <>
